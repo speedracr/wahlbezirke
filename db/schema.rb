80e0e904a4869ae6bf20bee99a7c601cc313fda2
Jonas Knipper <jonas@robocomobo.com>
2017-10-07 22:11:39 +0200

    Set up link with identifier

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20171007164408) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "user_name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
